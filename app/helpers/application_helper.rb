62cd386bc581d1488c20ad8a206f6c44ea6fa821
Mitch Loar <mitchloarweb@gmail.com>
2015-10-27 22:23:58 -0700

    Revert back to checkpoint complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,5 @@ module ApplicationHelper
     "Any Helper Method is a method that executes ruby code, but generally returns HTML.  
     It can also be used for conditional logic in the view... and other stuff..."
   end
-  
 
 end
