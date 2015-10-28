16856cc768b2839fe382e43654d5632bf9827b99
Mitch Loar <mitchloarweb@gmail.com>
2015-10-27 20:52:46 -0700

    Assignment 29 half complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,13 @@
 module ApplicationHelper
 
   def my_name
-    content_tag "H1", "Rick Ross"
+    content_tag "H3", "Rick Ross"
   end
-  def test_method
-
-    " hey world whats up"
-
-  end
-
 
   def any_helper_method
     "Any Helper Method is a method that executes ruby code, but generally returns HTML.  
     It can also be used for conditional logic in the view... and other stuff..."
   end
+  
+
 end
