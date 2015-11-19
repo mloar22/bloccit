2cc4b113e850fd4c3b1f80d93a852c7c8efd99c8
Mitch Loar <mitchloarweb@gmail.com>
2015-11-19 16:58:03 -0700

    config app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  # http://bloccit.herokuapp.com
+    config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
@@ -42,7 +45,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
