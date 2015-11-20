0d5a452d84b4b59ca7e3a15c40574e6c235e3fa0
Mitch Loar <mitchloarweb@gmail.com>
2015-11-19 17:04:41 -0700

    config app for Production emails, but this time for reals

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # http://bloccit.herokuapp.com
-    config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+    config.action_mailer.default_url_options = { host: 'bloccit-themitchedition.herokuapp.com' }
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
