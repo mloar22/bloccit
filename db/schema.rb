119d17649dec7511aaa8a9515717e9c769147219
Mitch Loar <mitchloarweb@gmail.com>
2015-11-23 22:40:56 -0700

    config app for production mails

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117195157) do
+ActiveRecord::Schema.define(version: 20151123204151) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 20151117195157) do
     t.string   "title"
     t.text     "body"
     t.boolean  "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "summaries", force: :cascade do |t|
+    t.string   "public"
+    t.string   "boolean"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
