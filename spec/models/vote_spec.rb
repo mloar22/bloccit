a18c8d2c4c41228ac512b9043acd4097c01da69d
Mitch Loar <mitchloarweb@gmail.com>
2016-02-01 13:33:45 -0700

    Vote assignment complete

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-class vote
+class Vote
   def valid?
       (@vote == 1) || (@vote == -1)
   end
@@ -20,12 +20,3 @@ describe "validations" do
     end
   end
 end
-#   describe "validations" do
-#     describe "value validation" do
-#       it "only allows -1 or 1 as values" do
-#         expect(vote.value).to eq(value)
-#
-#       end
-#     end
-#   end
-# end
