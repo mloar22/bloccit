943f6cd6233bd98f6964ad68f41f4c55f897a3ad
Mitch Loar <mitchloarweb@gmail.com>
2016-03-08 16:53:10 -0700

    checkpoint 55 complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,20 +3,20 @@ Rails.application.routes.draw do
   get 'comments/create'
 
    devise_for :users
-   resources :users, only: [:update, :show]
+   resources :users, only: [:update, :show, :index]
 
    resources :topics do
-     resources :posts, except: [:index] do
+     resources :posts, except: [:index], controller: 'topics/posts'
        resources :comments, only: [:create, :destroy]
        resources :favorites, only: [:create, :destroy]
 
        post '/up-vote' => 'votes#up_vote', as: :up_vote
        post '/down-vote' => 'votes#down_vote', as: :down_vote
-    end
+
      resources :summaries
    end
 
-   resources :posts, only: [] do
+   resources :posts, only: [:index] do
      resources :favorites, only: [:create, :destroy]
    end
 
